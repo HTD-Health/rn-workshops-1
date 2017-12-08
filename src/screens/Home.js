4eb798c5567456158769b0ddf7369ceeb7817133
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2017-12-08 23:52:01 +0100

    Add Button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { View } from 'react-native'
-import Button from '../components/Button'
 import { openAddMealScreen } from '../lib/NavigationHelpers'
 
 class Home extends React.Component {
   render () {
     return (
       <View style={{flex: 1}}>
-        <Button title="Add new meal" onPress={() => openAddMealScreen(this.props.navigation)} />
+       
       </View>
     )
   }
